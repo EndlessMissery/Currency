0e62df4a3dbc7e7068faeadcffbe985e43116ca1
EndlessMissery <111017439+EndlessMissery@users.noreply.github.com>
2023-02-20 21:08:32 +0100

    Update script.js

--- JavaScript/JS/script.js
+++ JavaScript/JS/script.js
@@ -8,18 +8,3 @@
 < menší (<=)
 > větší (>=)
 
-
-*/
-
-let jobName = "coder"
-
-if(jobName === "coder") {
-    console.log ("Jaký je rozdíl mezi HTML, CSS a JS?")
-}
-else if(jobName === "secretary") {
-    console.log ("Jaké máte zkušenosti v Excelu?")
-}
-else if(jobName === "administrator") {
-    console.log ("Jaké znáte bezpečnostní programy?")
-
-}
