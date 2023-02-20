d9343d0ec7d1f8de26619c6d992a0a9b791b43e3
EndlessMissery <111017439+EndlessMissery@users.noreply.github.com>
2023-02-20 21:05:35 +0100

    Add files via upload

---
+++ JavaScript/JS/script.js
@@ -0,0 +1,25 @@
+
+
+
+
+/*
+=== rovná se
+!== nerovná se
+< menší (<=)
+> větší (>=)
+
+
+*/
+
+let jobName = "coder"
+
+if(jobName === "coder") {
+    console.log ("Jaký je rozdíl mezi HTML, CSS a JS?")
+}
+else if(jobName === "secretary") {
+    console.log ("Jaké máte zkušenosti v Excelu?")
+}
+else if(jobName === "administrator") {
+    console.log ("Jaké znáte bezpečnostní programy?")
+
+}
